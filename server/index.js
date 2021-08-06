ebd0d65835af864711185fbdcd15d3e438022184
mrvnvlkmnn <marvin.volkmann.mv@gmail.com>
2021-08-06 20:24:01 +0200

    finished setting up db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'blog' ,
+});
 
 app.get("/", (req, res) => {
-    res.send("test")
-})
+
+});
 
 
 app.listen(3001, () => {
-    console.log("running on port 3001")
-})
+    console.log("running on port 3001");
+});
