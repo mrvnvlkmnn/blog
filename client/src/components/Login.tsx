649898a03178191d2524775fca8085945881051c
mrvnvlkmnn <marvin.volkmann.mv@gmail.com>
2021-08-07 18:17:12 +0200

    perpared loginpage for backend

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { InputText } from 'primereact/inputtext';
+import Axios from "axios";
 
 interface Props {
     darkMode: boolean
@@ -7,20 +8,24 @@ interface Props {
 
 export const Login = (props: Props) => {
 
-    const handleSubmit = (event: any) => {
-        alert("Test");
-        event.preventDefault();
+    const submitLogin = () => {
+        Axios.post("http://localhost:3001/api/login", [userName, password])
     }
+    const [userName, setUsername] = useState("");
+    const [password, setPasword] = useState("");
 
     return(
         <div className="login-wrapper">
             <div className="login">
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="username">Username</label>
-                    <InputText id="username" />
-                    <label htmlFor="password">Password</label>
-                    <InputText id="password" />
-                </form>
+                <label htmlFor="username">Username</label>
+                <InputText id="username" onChange={(e) => {
+                    setUsername(e.target.value);
+                }}/>
+                <label htmlFor="password">Password</label>
+                <InputText id="password" onChange = {(e) => {
+                    setPasword(e.target.value);
+                }}/>
+                <button onClick={submitLogin}>Login</button>
             </div>
         </div>
     )
