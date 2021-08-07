b0ec4d8bdbc788aed7d0e633c86a4b3ea93e9325
lars <lars.barnekow@gmx.de>
2021-08-08 01:25:04 +0200

    Even more CSS

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,9 +7,11 @@ interface Props {
 
 export const Header = (props: Props) => {
 
+    const darkmode = `${props.darkMode ? 'darkmode' : ''}`
+
     return(
         <div className="header-wrapper">
-            <div className={`header ${props.darkMode ? 'darkmode' : ''}`}>
+            <div className={`header ${darkmode}`}>
                 <NavLink to="/" className="logo">Blog</NavLink>
                 <input className="menu-btn" type="checkbox" id="menu-btn" />
                 <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
@@ -18,7 +20,7 @@ export const Header = (props: Props) => {
                     <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/careers">Careers</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
-                    <li><NavLink to="/login"><span className="pi pi-user" /></NavLink></li>
+                    <li><NavLink className={`user-login-animation ${darkmode}`} to="/login"><span className="pi pi-user" /></NavLink></li>
                 </ul>
             </div>
         </div>
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Button } from 'primereact/button';
 
-export const Home = () => {
+interface Props {
+    darkMode: boolean;
+}
+
+export const Home = (props: Props) => {
+
+    const darkmode = `${props.darkMode ? "darkmode" : ""}`
+
     return(
         <div className="home-wrapper">
-            <div className="home">
+            <div className={`home ${darkmode}`}>
                 Test
             </div>
         </div>
