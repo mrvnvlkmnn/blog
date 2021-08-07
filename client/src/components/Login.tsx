5543df7034e775f4a0fc7ca8b1b5c1af336f6b8b
lars <lars.barnekow@gmx.de>
2021-08-07 19:50:32 +0200

    Login-Screen Mask

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { InputText } from 'primereact/inputtext';
+import { Checkbox } from 'primereact/checkbox';
 
 interface Props {
     darkMode: boolean
 }
 
 export const Login = (props: Props) => {
+    const [rememberMe, setRememberMe] = React.useState(false);
 
     const handleSubmit = (event: any) => {
         alert("Test");
@@ -15,11 +17,18 @@ export const Login = (props: Props) => {
     return(
         <div className="login-wrapper">
             <div className="login">
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="username">Username</label>
-                    <InputText id="username" />
-                    <label htmlFor="password">Password</label>
-                    <InputText id="password" />
+                <form className="login-form" onSubmit={handleSubmit}>
+                    <span className="p-float-label">
+                        <InputText id="username" />
+                        <label htmlFor="username">Username</label>
+                    </span>
+                    <span className="p-float-label">
+                        <InputText id="password" />
+                        <label htmlFor="password">Password</label>
+                    </span>
+                    <div>
+                        <Checkbox checked={rememberMe} onChange={() => setRememberMe(currentState => !currentState)} />&nbsp;Remember&nbsp;Me
+                    </div>
                 </form>
             </div>
         </div>
