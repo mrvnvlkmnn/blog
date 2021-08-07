afb12754d3a9495dda056079e60eb15ae63d1d16
mrvnvlkmnn <marvin.volkmann.mv@gmail.com>
2021-08-07 17:06:20 +0200

    more server settings

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
@@ -9,8 +11,19 @@ const db = mysql.createPool({
     database: 'blog' ,
 });
 
-app.get("/", (req, res) => {
+app.use(cors);
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.post("/api/insertPost", (req, res) => {
+    const user_id = req.body.user_id;
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const sqlInsert = "INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)"
+    mysql.query(sqlInsert, [user_id, title, content], (err, result) => {
+        console.log(err)
+    } )
 });
 
 
