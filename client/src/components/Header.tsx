e7d4238e0aca3c0fb37ab60d25bb4fcda64457cf
Lars <lars.barnekow@gmx.de>
2021-08-14 22:54:07 +0200

    small Changes

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,34 +6,20 @@ interface Props {
 
 export const Header = (props: Props) => {
 
+    const [checked, setChecked] = React.useState(false);
+
     return(
-        // <header className="header">
-        //     <div>
-        //         <input type="checkbox" className="menu-btn" id="menu-btn" />
-        //         <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
-        //     </div>
-        //     <div className="logo">
-        //         <NavLink to="/">Blog</NavLink>
-        //     </div>
-        //     <ul className="header-links">
-        //         <li><NavLink to="/">Home</NavLink></li>
-        //         <li><NavLink to="/about">About</NavLink></li>
-        //         <li><NavLink to="/careers">Careers</NavLink></li>
-        //         <li><NavLink to="/contact">Contact</NavLink></li>
-        //         <li><NavLink to="/login"><span className="pi pi-user"/></NavLink></li>
-        //     </ul>
-        // </header>
         <div className="header-wrapper">
             <div className="header">
                 <NavLink to="/" className="logo">Blog</NavLink>
-                <input className="menu-btn" type="checkbox" id="menu-btn" />
+                <input className="menu-btn" type="checkbox" id="menu-btn" checked={checked} onClick={() => setChecked(currentState => !currentState)}/>
                 <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
                 <ul className="menu">
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/careers">Careers</NavLink></li>
-                    <li><NavLink to="/contact">Contact</NavLink></li>
-                    <li><NavLink to="/login"><span className="pi pi-user"/></NavLink></li>
+                    <li><NavLink to="/" onClick={() => setChecked(false)}>Home</NavLink></li>
+                    <li><NavLink to="/about" onClick={() => setChecked(false)}>About</NavLink></li>
+                    <li><NavLink to="/careers" onClick={() => setChecked(false)}>Careers</NavLink></li>
+                    <li><NavLink to="/contact" onClick={() => setChecked(false)}>Contact</NavLink></li>
+                    <li><NavLink to="/login" onClick={() => setChecked(false)}><span className="pi pi-user"/></NavLink></li>
                 </ul>
             </div>
         </div>
