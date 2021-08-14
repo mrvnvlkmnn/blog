1bddc6a61ec00c73c95a2a43823c2fbff00278f4
Lars <lars.barnekow@gmx.de>
2021-08-14 22:48:17 +0200

    mobile Ansicht

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -7,30 +7,35 @@ interface Props {
 export const Header = (props: Props) => {
 
     return(
-        <header className="header">
-            {/* <div className="responsive">
-                <input type="checkbox" />
-                <span className="burger" />
-                <span className="burger" />
-                <span className="burger" />
-                <ul className="header-links-res">
+        // <header className="header">
+        //     <div>
+        //         <input type="checkbox" className="menu-btn" id="menu-btn" />
+        //         <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+        //     </div>
+        //     <div className="logo">
+        //         <NavLink to="/">Blog</NavLink>
+        //     </div>
+        //     <ul className="header-links">
+        //         <li><NavLink to="/">Home</NavLink></li>
+        //         <li><NavLink to="/about">About</NavLink></li>
+        //         <li><NavLink to="/careers">Careers</NavLink></li>
+        //         <li><NavLink to="/contact">Contact</NavLink></li>
+        //         <li><NavLink to="/login"><span className="pi pi-user"/></NavLink></li>
+        //     </ul>
+        // </header>
+        <div className="header-wrapper">
+            <div className="header">
+                <NavLink to="/" className="logo">Blog</NavLink>
+                <input className="menu-btn" type="checkbox" id="menu-btn" />
+                <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+                <ul className="menu">
                     <li><NavLink to="/">Home</NavLink></li>
                     <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/careers">Careers</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
                     <li><NavLink to="/login"><span className="pi pi-user"/></NavLink></li>
                 </ul>
-            </div> */}
-            <div className="logo">
-                <NavLink to="/">Blog</NavLink>
             </div>
-            <div className="header-links">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/careers">Careers</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-                <NavLink to="/login"><span className="pi pi-user"/></NavLink>
-            </div>
-        </header>
+        </div>
     )
 }
